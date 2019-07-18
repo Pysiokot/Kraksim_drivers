f8da9ea10429ff819409a36437a181b98dbb7404
MiRudnik <michalrudnik1@gmail.com>
2019-07-18 09:16:53 +0200

    New beautiful car colors

--- src/main/java/pl/edu/agh/cs/kraksim/main/drivers/Driver.java
+++ src/main/java/pl/edu/agh/cs/kraksim/main/drivers/Driver.java
@@ -16,17 +16,20 @@ public abstract class Driver implements Comparable<Driver> {
 	protected int departureTurn;
 	protected Color carColor;
 	protected boolean emergency;
+	private final static Color[] carColors = {new Color(0xff60a917), new Color(0xff008a00), new Color(0xff1ba1e2),
+			new Color(0xff0050ef), new Color(0xffaa00ff), new Color(0xfff472d0), new Color(0xffd80073),
+			new Color(0xffa20025), new Color(0xffe51400), new Color(0xfffa6800), new Color(0xfff0a30a),
+			new Color(0xffe3c800),	new Color(0xff825a2c)};
 
 	protected Driver(int id, TravellingScheme scheme, Router router, boolean emergency) {
 		this.id = id;
 		this.router = router;
 		cursor = scheme.cursor();
 		if (emergency) {
-			carColor = Color.WHITE; // scheme.getEmergencyVehicleColor();
+			carColor = Color.WHITE;
 		} else {
-			carColor = scheme.getDriverColor();
 			Random generator = new Random();
-			this.carColor = new Color(generator.nextFloat(), generator.nextFloat(), generator.nextFloat());
+			this.carColor = carColors[generator.nextInt(carColors.length)];
 		}
 		this.emergency = emergency;
 	}
